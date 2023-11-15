8329fb6b0a439fbc35a72235ab17eac94fb61f4c
mohamadsafik <safikmohamad14@gmail.com>
2023-11-15 12:53:48 +0700

    Initial Commit

---
+++ peternakan-sapi/peternakan_sapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  peternakan_sapiApp.swift
+//  peternakan-sapi
+//
+//  Created by Safik Mohamad on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct peternakan_sapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
